a10b7372134bfc9b128ca3ad74bf5876804c50b5
mikesuprise1 <mikesupris1@gmail.com>
2017-01-10 14:27:19 -0700

    Added erb forms to portfolio

--- db/migrate/20170109234220_create_portfolios.rb
+++ db/migrate/20170109234220_create_portfolios.rb
@@ -1,6 +1,6 @@
-class CreatePortforlios < ActiveRecord::Migration[5.0]
+class CreatePortfolios < ActiveRecord::Migration[5.0]
   def change
-    create_table :portforlios do |t|
+    create_table :portfolios do |t|
       t.string :title
       t.string :subtitle
       t.text :body
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109174220) do
+ActiveRecord::Schema.define(version: 20170109234220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170109174220) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "portforlios", force: :cascade do |t|
+  create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
     t.text     "body"
