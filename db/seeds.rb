d39b2b40b71510278c551253c2b972e19ec0d144
mikesuprise1 <mikesupris1@gmail.com>
2017-02-07 00:38:53 -0700

    Updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+	email: "test@test.com",
+	password: "asdfasdf",
+	password_confirmation: "asdfasdf",
+	name: "Admin User",
+	roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+	email: "test2@test.com",
+	password: "asdfasdf",
+	password_confirmation: "asdfasdf",
+	name: "Regular User",
+)
+
+puts "1 Admin user created"
+
 Topic.create!(title: "asdasdasd")
 
 5.times do |blog|
